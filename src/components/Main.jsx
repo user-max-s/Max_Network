dffe792f61304c7a935cb8274a5d091ccc530255
Max S <maxtopstrilets@gmail.com>
2020-08-25 13:15:20 +0300

    added a component folder and split our App.js

---
+++ src/components/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main className="main">
+            <img src="https://www.klaviyo.com/wp-content/uploads/2016/09/abstract-background-1024x273.jpg" />
+            <div className="main__profile-info">
+                <img src="" />
+                <div className="">
+                    <h2>Dmitriy K.</h2>
+                    <p>Date of Birth: 2 january</p>
+                    <p>City: Minsk</p>
+                    <p>Education: BSU*11</p>
+                    <p>Web Site: https//:google.com</p>
+                </div>
+                <div className="main__posts">
+                    <div className="add-post">
+                        <h2>My posts</h2>
+                        <textarea></textarea>
+                        <button>Send</button>
+                    </div>
+                    <div className="post">
+                        <img src="" />
+                        <p>Hey why nobody love me?</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <ul className="menu">
+                <li><a href="#" className="menu__item">Profile</a></li>
+                <li><a href="#" className="menu__item">Messages</a></li>
+                <li><a href="#" className="menu__item">News</a></li>
+                <li><a href="#" className="menu__item">Music</a></li>
+                <li><a href="#" className="menu__item">Settings</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar; 
